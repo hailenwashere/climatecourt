9195c872943ad66c0475504c50c0c972cf9f47f7
Grace Y <grace.shaw.yan@gmail.com>
2025-04-26 15:02:24 -0700

    styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       }}
     >
       <main className="flex flex-col items-center gap-[32px] mb-auto">
-        <PlaintiffVsDefendant />
         <CourtTitle />
+        <PlaintiffVsDefendant />
         <ConfessionBox />
 
         <div className="flex justify-between w-full max-w-2xl">
@@ -24,6 +24,14 @@ export default function Home() {
     </div>
   );
 }
+const CourtTitle = () => {
+  return (
+    <div className="flex flex-col gap-[8px] text-9xl italic font-extrabold text-center">
+        <div className="bg-yellow-400 px-8">CLIMATE</div>
+        <div className="bg-yellow-400 px-8">COURT</div>
+      </div>
+  );
+};
 
 const PlaintiffVsDefendant = () => {
   return (
@@ -41,14 +49,7 @@ const PlaintiffVsDefendant = () => {
   );
 };
 
-const CourtTitle = () => {
-  return (
-    <div className="flex flex-col gap-[8px] text-9xl italic font-extrabold text-center">
-        <div className="bg-yellow-400 px-8">CLIMATE</div>
-        <div className="bg-yellow-400 px-8">COURT</div>
-      </div>
-  );
-};
+
 
 const ConfessionBox = () => {
   const [confession, setConfession] = useState<string>("");
@@ -62,18 +63,18 @@ const ConfessionBox = () => {
   };
 
   return (
-    <div className="w-full max-w-2xl mt-8">
+    <div className="w-full max-w-2xl mt-8 mb-8">
       <form onSubmit={handleSubmit} className="flex flex-col items-center">
         <textarea
           value={confession}
           onChange={(e) => setConfession(e.target.value)}
           placeholder="confess your climate crime.."
-          className="w-full p-4 min-h-[100px] border border-gray-300 rounded-md bg-white text-xl font-serif"
+          className="w-full p-20 min-h-[100px] border border-gray-300 rounded-lg bg-white text-2xl font-serif"
           required
         />
         <button
           type="submit"
-          className="btn btn-warning mt-4 px-8 py-3 bg-yellow-400 text-black font-bold hover:bg-yellow-500"
+          className="btn btn-warning mt-4 mb-12 rounded-lg px-12 py-6 bg-yellow-400 text-black font-bold hover:bg-yellow-500"
         >
           submit to judgement
         </button>
@@ -84,7 +85,7 @@ const ConfessionBox = () => {
 
 const BreakingNewsButton = () => {
   return (
-    <button className="btn btn-outline border-2 border-gray-500 mb-4 rounded-md px-8 py-3 w-[300px]">
+    <button className="btn btn-outline border-2 border-gray-500 rounded-lg px-8 py-3 w-[300px]">
       breaking news
     </button>
   );
@@ -92,7 +93,7 @@ const BreakingNewsButton = () => {
 
 const JuryDutyButton = () => {
   return (
-    <button className="btn btn-outline border-2 border-gray-500 rounded-md px-8 py-3 w-[300px]">
+    <button className="btn btn-outline border-2 border-gray-500 rounded-lg px-8 py-3 w-[300px]">
       jury duty
     </button>
   );

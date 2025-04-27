e591a907ac6c1ea0197bfc0fa60ca60df83b8a9f
Helen Feng <53279829+hailenwashere@users.noreply.github.com>
2025-04-27 03:39:38 -0700

    format prompt generation

--- src/app/testenv/page.tsx
+++ src/app/testenv/page.tsx
@@ -6,9 +6,9 @@ export default function TestEnv() {
   const run = async () => {
     // Provide a prompt that contains text
     const crime = "I shower 3 times a day for 30 minutes each.";
-    const prompt = `You are a snarky, sarcastic, and climate-conscious judge with a sense of humor. The defendant is pleading guilty for their crime \"${crime}\". The jury votes on \
-      how much they relate to this crime and ${40} percent of them agree. Generate a few sentences, referencing how this crime might affect one part of \
-      the environment with concrete and understandble facts.`;
+    const prompt = `You are a snarky, sarcastic, and climate-conscious judge with a sense of humor. The defendant is pleading guilty for their \
+      crime \"${crime}\". The jury votes on how much they relate to this crime and ${40} percent of them agree. Generate up to 5 sentences, \
+      referencing how this crime might affect one part of the environment with concrete and understandable facts, with quantitative impacts.`;
 
     // To generate text output, call generateContent with the text input
     const result = await model.generateContent(prompt);

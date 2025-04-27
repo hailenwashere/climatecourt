a1f141dad1bf85285468563a6cbccba521aa100e
ghtjason <jasonshuhezhang@gmail.com>
2025-04-26 23:23:46 -0700

    monitor

--- src/app/courtroom/page.tsx
+++ src/app/courtroom/page.tsx
@@ -1,26 +1,27 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
-import { use, useEffect, useState } from "react";
-import { DatabaseReference, getDatabase, increment, onValue, ref, set, get, update } from "firebase/database"; 
-import {db} from "@/app/lib/firebase"; // assuming you already set up firebase like your earlier message
+import { useEffect, useState } from "react";
+import { increment, onValue, ref, get, update } from "firebase/database";
+import { db } from "@/app/lib/firebase"; // assuming you already set up firebase like your earlier message
 // import Image from "next/image";
 import NPCImage from "@/app/components/NpcImage";
 import UserAvatar from "@/app/components/UserAvatar";
 
 export default function Courtroom() {
   interface CourtroomLogic {
-    secondsLeft: number,
-    numPpl: number,
-    currCrime: string
-    yayCount: number,
-    nayCount: number
+    secondsLeft: number;
+    numPpl: number;
+    currCrime: string;
+    yayCount: number;
+    nayCount: number;
   }
 
   const [vote, setVote] = useState<"yay" | "nay" | null>(null);
   const [hasVoted, setHasVoted] = useState(false);
   const [courtroom, setCourtroom] = useState<CourtroomLogic>();
   // const [seconds, setSeconds] = useState();
-  const [crime, setCrime] = useState<String>();
+  const [crime, setCrime] = useState<string>();
   // const [numPpl, setNumPpl] = useState(1);
 
   const handleVote = async (newVote: "yay" | "nay") => {
@@ -30,44 +31,42 @@ export default function Courtroom() {
 
   useEffect(() => {
     const courtroomLogicRef = ref(db, "logic");
-  
+
     const changeVote = onValue(courtroomLogicRef, async (snapshot) => {
       const newCourtroom = snapshot.val();
       setCourtroom(newCourtroom);
-  
+
       if (newCourtroom?.currCrime !== undefined) {
         const crimeRef = ref(db, `crimes/${newCourtroom.currCrime}`);
         const crimeSnapshot = await get(crimeRef);
         setCrime(crimeSnapshot.val().crime);
       }
     });
-  
+
     return () => changeVote();
   }, []);
-  
 
   useEffect(() => {
     // Update Firebase Realtime Database
     const updateVotes = async () => {
       if (!courtroom) {
         return;
-      }
-      else {
+      } else {
         const crimeRef = ref(db, `crimes/${courtroom.currCrime}`);
         const courtroomRef = ref(db, "logic");
-        let yesIncrement = 0;
-        let noIncrement = 0;
-        vote === "yay" ? yesIncrement = 1 : noIncrement = 1;
-        if (hasVoted) {
-          const courtroom = (await get(courtroomRef)).val();
-          yesIncrement = vote === "yay" ? 1 : (courtroom.yayCount > 0 ? -1 : 0);
-          noIncrement  = vote === "nay" ? 1 : (courtroom.nayCount > 0 ? -1 : 0);
-        }
+
+        const decrement = hasVoted ? -1 : 0;
+        const yesIncrement = vote === "yay" ? 1 : decrement;
+        const noIncrement = vote === "yay" ? decrement : 1;
+
         setHasVoted(true);
-        
+
         // updating realtime courtroom data
-        await update(courtroomRef, {yayCount: increment(yesIncrement), nayCount: increment(noIncrement)});
-        
+        await update(courtroomRef, {
+          yayCount: increment(yesIncrement),
+          nayCount: increment(noIncrement),
+        });
+
         // updating crime (should be in firestore)
         const updatedCourtroomData = (await get(courtroomRef)).val();
         await update(crimeRef, {
@@ -80,52 +79,62 @@ export default function Courtroom() {
   }, [vote]);
 
   return (
-    <div className="grid grid-rows-[1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-serif)]">
-      <main className="flex flex-col items-center gap-8">
-        
-        {/* Card for Crime */}
-        <div className="bg-[url('/judge.webp')] bg-cover bg-center w-full max-w-[700px] h-[400px] flex flex-col justify-between items-center p-8 rounded-lg shadow-lg">
-          <div className="flex flex-col items-center gap-4">
-            <div className="text-3xl font-bold font-underline text-center">Today's Court Case:</div>
-            <div className="italic max-w-[600px] max-h-[150px] break-words text-center">
-              {crime ? crime : "Loading..."}
-            </div>
+    <div className="flex flex-col justify-center items-center h-full min-h-screen gap-16 font-[family-name:var(--font-serif)]">
+      {/* Card for Crime */}
+      <div className="bg-[url('/judge.webp')] bg-cover bg-center w-[600px] h-[400px] flex flex-col justify-between items-center p-8 rounded-lg shadow-lg relative">
+        <img
+          src="/monitor.png"
+          alt="Monitor"
+          width="670px"
+          className="max-w-none absolute left-1/2 -top-4 transform -translate-x-1/2 pointer-events-none"
+        />
+        <div className="flex flex-col items-center gap-4">
+          <div className="text-3xl font-bold font-underline text-center w-[600px] bg-red-500 text-white rounded-lg p-2 font-sans whitespace-nowrap">
+            Is This Moral in our Current Climate Catastrophe?
           </div>
-          <div className="flex flex-row justify-between items-center mb-25 w-4/5">
-            <div className ="flex flex-col items-center"> 
-              {courtroom ? `${courtroom.yayCount} Yays` : "Loading..."}
-              <button className={`btn text-xl ${vote === "yay" ? "btn-success" : "btn-outline"}`} onClick={() => handleVote("yay")}>
-                Yay 👍
-              </button>
-            </div>
-            <div className ="flex flex-col items-center"> 
-              {courtroom ? `${courtroom.nayCount} Nays` : "Loading..."}
-              <button className={`btn text-xl ${vote === "nay" ? "btn-error" : "btn-outline"}`} onClick={() => handleVote("nay")}>
-                Nay 👎
-              </button>
-            </div>
+          <div className="italic max-w-[600px] max-h-[150px] break-words text-center">
+            {crime ? crime : "Loading..."}
           </div>
         </div>
-
-        {/* Crowd */}
-        <div className="relative w-[600px] h-[300px] flex justify-center">
-          <NPCImage type="A" x={100} y={100} opacity={0.5} />
-          <NPCImage type="A" x={200} y={100} opacity={0.5} />
-          <NPCImage type="A" x={300} y={100} opacity={0.5} />
-          <NPCImage type="A" x={400} y={100} opacity={0.5} />
-
-          <NPCImage type="A" x={50} y={50} opacity={1} />
-          <NPCImage type="A" x={150} y={50} opacity={1} />
-          <NPCImage type="A" x={250} y={50} opacity={1} />
-          <NPCImage type="A" x={350} y={50} opacity={1} />
-          <UserAvatar x={450} y={50} vote={vote}/>
+        <div className="flex flex-row justify-between items-center mb-25 w-4/5">
+          <div className="flex flex-col items-center">
+            {courtroom ? `${courtroom.yayCount} Yays` : "Loading..."}
+            <button
+              className={`btn text-xl ${
+                vote === "yay" ? "btn-success" : "btn-outline"
+              }`}
+              onClick={() => handleVote("yay")}
+            >
+              Yay 👍
+            </button>
+          </div>
+          <div className="flex flex-col items-center">
+            {courtroom ? `${courtroom.nayCount} Nays` : "Loading..."}
+            <button
+              className={`btn text-xl ${
+                vote === "nay" ? "btn-error" : "btn-outline"
+              }`}
+              onClick={() => handleVote("nay")}
+            >
+              Nay 👎
+            </button>
+          </div>
         </div>
+      </div>
 
-      </main>
+      {/* Crowd */}
+      <div className="relative w-[600px] h-[300px] flex justify-center">
+        <NPCImage type="A" x={100} y={100} opacity={0.5} />
+        <NPCImage type="A" x={200} y={100} opacity={0.5} />
+        <NPCImage type="A" x={300} y={100} opacity={0.5} />
+        <NPCImage type="A" x={400} y={100} opacity={0.5} />
 
-      <footer className="row-start-2 text-sm">
-        made at LA Hacks 2025 by Helen Feng, Andrew Wang, Grace Yan, and Jason Zhang
-      </footer>
+        <NPCImage type="A" x={50} y={50} opacity={1} />
+        <NPCImage type="A" x={150} y={50} opacity={1} />
+        <NPCImage type="A" x={250} y={50} opacity={1} />
+        <NPCImage type="A" x={350} y={50} opacity={1} />
+        <UserAvatar x={450} y={50} vote={vote} />
+      </div>
     </div>
   );
 }
